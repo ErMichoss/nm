0d068f93bcd58d3fb39455e240c5cc051389162e
Nicolas Gonzalez De Mendoza <nicgonza@c1r13s1.42madrid.com>
2025-07-26 12:36:24 +0200

    starting to get the hang of it

--- src/main.c
+++ src/main.c
@@ -5,30 +5,33 @@
  * @param filename The binary's name
  * @return on success it returns 0, on failure 1
  */
-int ft_check_binary(char *filename) { //TODO Debemos almacenar el error, porque se imprimen al final de la ejecucion
+int ft_check_binary(char *filename, t_data* data) { //TODO Analizar tabla de symbolos, Limpieza, esos son los paso.
     int         fd;
     struct stat buf;
     void        *map;
-    const char  *err;
 
     fd = open(filename, O_RDONLY);
     if (fd == -1) {
-        write(2, "The file can not be opened\n", 28);
+        data->error = "The file can not be opened\n";
+        return 1;
+    }
+
+    if (check_if_elf(fd) == -1){
+        data->error == "Not an ELF file\n";
+        close(fd);
         return 1;
     }
 
     if (fstat(fd, &buf) == -1) {
-        err = strerror(errno);
-        write(2, err, ft_strlen(err));
-        write(2, "\n", 1);
+        data->error = strerror(errno);
+        close(fd);
         return 1;
     }
 
     map = mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (map == MAP_FAILED){
-        err = strerror(errno);
-        write(2, err, ft_strlen(err));
-        write(2, "\n", 1);
+        data->error = strerror(errno);
+        close(fd);
         return 1;
     }
 }
@@ -38,22 +41,32 @@ int ft_check_binary(char *filename) { //TODO Debemos almacenar el error, porque 
  * @param filename The binary's name
  * @return on success 0, on failure 1
  */
-int ft_get_binary_data(char *filename) {
-    //TODO agregar la struct y pasarsela a ft_check_binary para que se inicialice, desde la struct empezare con el programa de verdad
-
-    if (ft_check_binary(filename) == 1) {
+int ft_get_binary_data(char *filename, t_data* data) {
+    if (ft_check_binary(filename, data) == 1) {
         return 1;
     }
 }
 
 int main(int argc, char *argv[]) {
+    t_data* data;
+
     if (argc < 2) {
         write(2, "You must intorduce at leat on file name\n", 41);
         return 1;
     }
 
-    for (int i = 1; argv[i] != NULL; i++) {
-        ft_get_binary_data(argv[i]);
+    data = malloc(sizeof(t_data) * (argc - 1));
+    if (!data) {
+        write(2, "Memory allocation failed\n", 26);
+        return 1;
     }
+
+    for (int i = 1; argv[i] != NULL; i++) {
+        init_data(&data[i - 1]);
+        ft_get_binary_data(argv[i], &data[i - 1]);
+    }
+
+    free(data);
+
     return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -12,3 +12,32 @@ int ft_strlen(char *str){
     }
     return i;
 }
+
+/**
+ * @brief Initialices de struc so its ready to be used
+ * @param data A pointer to the struct
+ */
+void init_data(t_data* data) {
+    data->error = NULL;
+}
+
+/**
+ * @brief Checks if the file is a ELF file.
+ * @param fd The file descriptor of the file
+ * @return If it is a ELF file it returns 0, if not it will return -1
+ */
+int check_if_elf(int fd){
+    unsigned char buf[4];
+    ssize_t bytes_read;
+
+    bytes_read = read(fd, buf, 4);
+    if (bytes_read < 4) {
+        return -1;
+    }
+
+    if (buf[0] == 0x7f && buf[1] == 'E' && buf[2] == 'L' && buf[3] == 'F') {
+        return 0;
+    } else {
+        return -1;
+    }
+}

202bd8e36592a52f3e10b280dd842d5533183a8b
Nicolas Gonzalez De Mendoza <nicgonza@c1r8s1.42madrid.com>
2025-08-16 11:25:02 +0200

    Added the diferent targets to reach

--- incl/nm.h
+++ incl/nm.h
@@ -7,99 +7,24 @@
  #include <sys/stat.h>
  #include <sys/mman.h>
  #include <errno.h>
- 
- #define EI_NIDENT 16
-
- //<-- ELF_64 typedefs
- /**
-  * @brief Unsigned 16-bit integer (2 bytes). Used for small fields like counts or indexes.
-  */
- typedef unsigned short Elf64_Half;
- /**
-  * @brief Unsigned 32-bit integer (4 bytes). Used for standard-sized integer fields.
-  */
- typedef unsigned int   Elf64_Word;
- /**
-  * @brief Unsigned 64-bit integer (8 bytes). Used for large size or offset fields.
-  */
- typedef unsigned long  Elf64_Xword;
- typedef unsigned long  Elf64_Addr;
- typedef unsigned long  Elf64_Off;
- 
- //<-- STRUCTS -->
-
- /**
-  * @brief This is the main header at the start of an ELF file. It contains essential info about the file format and where to find other parts
-  * @param e_indent ELF identification
-  * @param e_type File type
-  * @param e_machine Target architecture
-  * @param e_version ELF version
-  * @param e_entry Entry point address
-  * @param e_phoff Offset to program header table
-  * @param e_shoff Offset to section header table
-  * @param e_flags Processor specific flags
-  * @param e_ehsize ELF header size
-  * @param e_phentsize Size of each program header entry
-  * @param e_phnum Number of program header entries
-  * @param e_shentsize Size of each section header entry
-  * @param e_shnum Number of sections header entrues
-  * @param e_shstrndx Index of section header string table (section names)
- */  
- typedef struct s_Elf64_Ehdr{
-    unsigned char e_ident[EI_NIDENT];
-    Elf64_Half    e_type;
-    Elf64_Half    e_machine;
-    Elf64_Word    e_version;
-    Elf64_Addr    e_entry;
-    Elf64_Off     e_phoff;
-    Elf64_Off     e_shoff;
-    Elf64_Word    e_flags;
-    Elf64_Half    e_ehsize;
-    Elf64_Half    e_phentsize;
-    Elf64_Half    e_phnum;
-    Elf64_Half    e_shentsize;
-    Elf64_Half    e_shnum;
-    Elf64_Half    e_shstrndx;
- } Elf64_Ehdr;
-
- typedef struct s_Elf64_Shdr{
-    Elf64_Word  sh_name;
-    Elf64_Word  sh_type;
-    Elf64_Xword sh_flags;
-    Elf64_Addr  sh_addr;
-    Elf64_Off   sh_offset;
-    Elf64_Xword sh_size;
-    Elf64_Word  sh_link;
-    Elf64_Word  sh_info;
-    Elf64_Xword sh_addralign;
-    Elf64_Xword sh_entsize;
- } Elf64_Shdr;
-
- typedef struct s_Elf64_Sym{
-    Elf64_Word      st_name;
-    unsigned char   st_info;
-    unsigned char   st_other;
-    Elf64_Half      st_shndx;
-    Elf64_Addr      st_value;
-    Elf64_Xword     st_size;
- } Elf64_Sym;
+ #include <elf.h>
 
  typedef struct s_data
  {
-    char *        filename;
-    int           fd;
+    char*         filename;
+    size_t        filesize;
     struct stat   st;
-    void *        map;
+    void*         map;
 
-    Elf64_Ehdr *  ehdr;
-    Elf64_Shdr *  shdr;
-    char *        shstrtab;
+    Elf64_Ehdr*   ehdr;
+    Elf64_Shdr*   shdr;
+    char*         shstrtab;
 
-    Elf64_Sym *   symtab;
-    char *        strtab;
+    Elf64_Sym*    symtab;
+    char*         strtab;
     size_t        sym_count;
 
-    char *        error;
+    char*         error;
  } t_data;
  
 
--- src/main.c
+++ src/main.c
@@ -3,9 +3,10 @@
 /**
  * @brief Checks if the binary can be access correctly for the program
  * @param filename The binary's name
+ * @param data THe data struct
  * @return on success it returns 0, on failure 1
  */
-int ft_check_binary(char *filename, t_data* data) { //TODO Analizar tabla de symbolos, Limpieza, esos son los paso.
+int ft_check_binary(char *filename, t_data* data) {//TODO checker si es un archivo o un dir.
     int         fd;
     struct stat buf;
     void        *map;
@@ -29,20 +30,60 @@ int ft_check_binary(char *filename, t_data* data) { //TODO Analizar tabla de sym
     }
 
     map = mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+
     if (map == MAP_FAILED){
         data->error = strerror(errno);
         close(fd);
         return 1;
     }
+
+    data->map = map;
+    data->filesize = buf.st_size;
+
+    return 0;
 }
+
+/**
+ * @brief Checks the ELF header
+ * @param data The data struct
+ * @return on success 0, on failure 1
+ */
+int ft_read_efl_header(t_data* data) { //TODO, añadir para 32 bits una vez completado para 64.
+    Elf64_Ehdr* ehdr = (Elf64_Ehdr*)data->map;
+    
+    if (ehdr->e_ident[EI_CLASS] != ELFCLASS64) {
+        data->error = "The file is not 64bits\n";
+        return 1;
+    }
+
+    data->ehdr = ehdr;
+    return 0;
+}
+
+/** 
+ * Interpretar ELF header → validar e inicializar punteros.
+ *
+ * Acceder a la tabla de secciones (Elf64_Shdr).
+ *
+ * Usar .shstrtab para encontrar .symtab y .strtab.
+ *
+ * Leer símbolos desde .symtab (Elf64_Sym).
+ *
+ * Imprimirlos (imitando nm).
+*/
 
 /**
  * @brief It will reproduce nm return
  * @param filename The binary's name
+ * @param data The data struct
  * @return on success 0, on failure 1
  */
-int ft_get_binary_data(char *filename, t_data* data) {
+int ft_get_binary_data(char *filename, t_data* data) { //revisar endianness porsiaca
     if (ft_check_binary(filename, data) == 1) {
+        return 1;
+    }
+    if (ft_read_elf_header(data) == 1) {
         return 1;
     }
 }
@@ -51,7 +92,7 @@ int main(int argc, char *argv[]) {
     t_data* data;
 
     if (argc < 2) {
-        write(2, "You must intorduce at leat on file name\n", 41);
+        write(2, "ft_nm: «a.out»: No such file\n", 32);
         return 1;
     }
 
